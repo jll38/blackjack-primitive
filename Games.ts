cf85e903291c618b1401a17ae3b36e54907cfdc2
Julian Lechner <jll38@njit.edu>
2024-02-07 16:57:59 -0500

    create and setup wip Blackjack Game class

--- Games.ts
+++ Games.ts
@@ -1,9 +1,51 @@
+import { Hand } from "./Hand";
+import { Deck } from "./Deck";
+import { PlayingCard } from "./PlayingCard";
+import { DealerHand } from "./blackjack";
+import { BlackjackHand } from "./blackjack";
+
 class Game {
-  game = null;
+  game: string | null = null;
+  playerWin = 0;
+  dealerWin = 0;
+
+  turn = 0;
+
   start(): void {
+    console.clear();
     console.log("------------------------------");
     console.log("🃏 Welcome to Lechner Casino 🃏");
     console.log("------------------------------");
     console.log(`Your are playing: ${this.game}`);
+
+    const { cards, dealer, player } = this.initObjects();
+    this.initiateTurn(cards, dealer, player);
+  }
+  initiateTurn(cards: Deck, dealer: DealerHand, player: BlackjackHand): void {}
+
+  protected initObjects(): any {
+    return null;
+  }
+}
+
+export class Blackjack extends Game {
+  game = "Blackjack";
+
+  initiateTurn(cards: Deck, dealer: DealerHand, player: BlackjackHand): void {
+    if (this.turn === 0) {
+        dealer.hit(cards);
+        dealer.hit(cards);
+        console.log(`Dealer showing ${dealer.getTotalHandString}`)
+    }
+
+    this.turn++;
+  }
+
+  protected initObjects(): any {
+    const deck = new Deck();
+    deck.shuffle();
+    const dealer = new DealerHand();
+    const player = new BlackjackHand();
+    return { deck, dealer, player };
   }
 }
--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import { BlackjackHand, DealerHand } from "./blackjack";
 import { Deck } from "./Deck";
-const prompt = require("prompt-sync")({ sigint: true });
-const win_count = [0, 0]; //[Dealer,Player]
-let playing = false;
 
+import { Blackjack } from "./Games";
+
+const BlackjackGame = new Blackjack();
+
+BlackjackGame.start();
