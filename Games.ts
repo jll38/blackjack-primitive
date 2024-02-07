6615b70b28b94f3da632d3d54d95b4aa86b9247a
Julian Lechner <jll38@njit.edu>
2024-02-07 15:57:51 -0500

    create game class

---
+++ Games.ts
@@ -0,0 +1,9 @@
+class Game {
+  game = null;
+  start(): void {
+    console.log("------------------------------");
+    console.log("🃏 Welcome to Lechner Casino 🃏");
+    console.log("------------------------------");
+    console.log(`Your are playing: ${this.game}`);
+  }
+}
--- index.ts
+++ index.ts
@@ -3,27 +3,4 @@ import { Deck } from "./Deck";
 const prompt = require("prompt-sync")({ sigint: true });
 const win_count = [0, 0]; //[Dealer,Player]
 let playing = false;
-console.log("\nWelcome to Blackjack 🃏");
-prompt("Press any key to begin...");
 
-const deck = new Deck();
-deck.shuffle();
-
-const dealer = new DealerHand();
-const player = new BlackjackHand();
-
-dealer.hit(deck);
-dealer.hit(deck);
-
-console.log("\nDealer's Hand")
-console.log(dealer.getHand())
-player.hit(deck);
-player.hit(deck);
-
-console.log("\nPlayer's Hand");
-console.log(player.getHand())
-
-console.log("Player Hit")
-player.hit(deck);
-console.log(player.getHand());
-console.log(player.getTotalHand())
