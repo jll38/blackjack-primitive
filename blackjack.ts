6480595b37ae1ee2cf8e6dbc293143ea0a6f01ac
Julian Lechner <jll38@njit.edu>
2024-02-07 13:54:52 -0500

    index file for utilizing classes

--- blackjack.ts
+++ blackjack.ts
@@ -6,7 +6,6 @@ interface BlackjackHandProps {
   hit(deck: Deck): void;
   getTotalHand(): number[];
   getTotalHandString(): string;
-  getNumericalHand(): number[][];
 }
 
 export class BlackjackHand extends Hand implements BlackjackHandProps {
@@ -17,14 +16,11 @@ export class BlackjackHand extends Hand implements BlackjackHandProps {
     return this.totalHand;
   }
 
-  getNumericalHand(): number[][] {
-    return this.numericalHand;
-  }
-
   getTotalHandString(): string {
     const hand = this.getTotalHand();
     return `${hand[0]}${hand[1] && hand[1] <= 21 ? ` or ${hand[1]}` : ``}`;
   }
+  
   hit(deck: Deck): void {
     const card = deck.pop();
     if (card === undefined) throw new Error("No cards in the deck to draw.");
@@ -32,27 +28,20 @@ export class BlackjackHand extends Hand implements BlackjackHandProps {
     switch (card.getRank()) {
       case "Ace":
         this.totalHand[1] += 11;
-        this.numericalHand[1].push(11);
         if (this.totalHand[1] > 21 && this.totalHand[0] + 11 <= 21) {
           this.totalHand[0] += 11;
-          this.numericalHand[0].push(11);
         } else {
           this.totalHand[0] += 1;
-          this.numericalHand[0].push(1);
         }
         break;
       case "King":
       case "Queen":
       case "Jack":
         this.totalHand = this.totalHand.map((value) => value + 10);
-        this.numericalHand[0].push(10);
-        this.numericalHand[1].push(10);
         break;
       default:
         const convertedInt = Number.parseInt(card.getRank());
         this.totalHand = this.totalHand.map((value) => value + convertedInt);
-        this.numericalHand[0].push(convertedInt);
-        this.numericalHand[1].push(convertedInt);
         break;
     }
   }
@@ -61,23 +50,14 @@ export class BlackjackHand extends Hand implements BlackjackHandProps {
 export class DealerHand extends BlackjackHand {
   hidingCard = true;
 
-  getNumericalHand(): number[][] {
-    if (this.hidingCard)
-      return [this.numericalHand[0].slice(1), this.numericalHand[1].slice(1)];
-    return this.numericalHand;
+  getHand(): PlayingCard[] {
+    if (this.hidingCard) {
+      return this.cards.slice(1);
+    }
+    return this.cards;
   }
-
-  showCard(): void {
+  
+  showHand(): void {
     this.hidingCard = false;
-  }
-
-  getHandToString(): string {
-    if (this.hidingCard)
-      return `Dealer Showing ${
-        this.numericalHand[0][1] !== this.numericalHand[1][1]
-          ? `${this.numericalHand[1][1]} or ${this.numericalHand[0][1]}`
-          : this.numericalHand[0][1]
-      }`;
-    return `${this.totalHand[0] !== this.totalHand[1] ? `Showing ${this.totalHand[0]} or ${this.totalHand[1]}` : `Showing ${this.totalHand[0]}`}`
   }
 }
--- index.ts
+++ index.ts
@@ -3,33 +3,24 @@ import { Deck } from "./Deck";
 const prompt = require("prompt-sync")({ sigint: true });
 const win_count = [0, 0]; //[Dealer,Player]
 let playing = false;
-console.log("Welcome to Blackjack 🃏");
+console.log("\nWelcome to Blackjack 🃏");
 prompt("Press any key to begin...");
-playing = true;
 
 const deck = new Deck();
 const dealer = new DealerHand();
 const player = new BlackjackHand();
 
-while (playing) {
-  //Deal Initial Cards
-  dealer.hit(deck);
-  player.hit(deck);
-  dealer.hit(deck);
-  player.hit(deck);
+dealer.hit(deck);
+dealer.hit(deck);
 
-  //Display Cards
-  console.log(dealer.getHandToString());
-  console.log(player.getHandToString());
+console.log("\nDealer's Hand")
+console.log(dealer.getHand())
+player.hit(deck);
+player.hit(deck);
 
-  //Player Continue
-  var continueHit = prompt("Hit or Stand? (y/n)");
-  while (continueHit === "y") {
-    player.hit(deck);
-    console.log(player.getHandToString());
-    continueHit = prompt("Hit or Stand? (y/n)");
-  }
+console.log("\nPlayer's Hand");
+console.log(player.getHand())
 
-  dealer.showCard();
-  dealer.getHandToString();
-}
+console.log("Player Hit")
+console.log(player.getHand());
+console.log(player.getTotalHand())
