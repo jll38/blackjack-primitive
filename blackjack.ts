d3d84befa29ec5095a71332c8d14b2db32ea2c24
Julian Lechner <jll38@njit.edu>
2024-02-07 01:24:40 -0500

    Work on dealer functionality

--- blackjack.ts
+++ blackjack.ts
@@ -1,17 +1,24 @@
 import { Hand } from "./Hand";
 import { Deck } from "./Deck";
+import { PlayingCard } from "./PlayingCard";
 
 interface BlackjackHandProps {
   hit(deck: Deck): void;
   getTotalHand(): number[];
   getTotalHandString(): string;
+  getNumericalHand(): number[][];
 }
 
 export class BlackjackHand extends Hand implements BlackjackHandProps {
-  private totalHand = [0, 0];
+  totalHand = [0, 0];
+  numericalHand: number[][] = [[], []];
 
   getTotalHand(): number[] {
     return this.totalHand;
+  }
+
+  getNumericalHand(): number[][] {
+    return this.numericalHand;
   }
 
   getTotalHandString(): string {
@@ -20,25 +27,57 @@ export class BlackjackHand extends Hand implements BlackjackHandProps {
   }
   hit(deck: Deck): void {
     const card = deck.pop();
-    if(card === undefined) throw new Error("No cards in the deck to draw.");
+    if (card === undefined) throw new Error("No cards in the deck to draw.");
     this.push(card);
     switch (card.getRank()) {
       case "Ace":
         this.totalHand[1] += 11;
-        if(this.totalHand[1] > 21 && this.totalHand[0] + 11 <=21){
-            this.totalHand[0] += 11;
-        } else{
-            this.totalHand[0] += 1;
+        this.numericalHand[1].push(11);
+        if (this.totalHand[1] > 21 && this.totalHand[0] + 11 <= 21) {
+          this.totalHand[0] += 11;
+          this.numericalHand[0].push(11);
+        } else {
+          this.totalHand[0] += 1;
+          this.numericalHand[0].push(1);
         }
         break;
       case "King":
       case "Queen":
       case "Jack":
         this.totalHand = this.totalHand.map((value) => value + 10);
+        this.numericalHand[0].push(10);
+        this.numericalHand[1].push(10);
         break;
       default:
-        this.totalHand = this.totalHand.map((value) => value + Number.parseInt(card.getRank()));
+        const convertedInt = Number.parseInt(card.getRank());
+        this.totalHand = this.totalHand.map((value) => value + convertedInt);
+        this.numericalHand[0].push(convertedInt);
+        this.numericalHand[1].push(convertedInt);
         break;
     }
+  }
+}
+
+export class DealerHand extends BlackjackHand {
+  hidingCard = true;
+
+  getNumericalHand(): number[][] {
+    if (this.hidingCard)
+      return [this.numericalHand[0].slice(1), this.numericalHand[1].slice(1)];
+    return this.numericalHand;
+  }
+
+  showCard(): void {
+    this.hidingCard = false;
+  }
+
+  getHandToString(): string {
+    if (this.hidingCard)
+      return `Dealer Showing ${
+        this.numericalHand[0][1] !== this.numericalHand[1][1]
+          ? `${this.numericalHand[1][1]} or ${this.numericalHand[0][1]}`
+          : this.numericalHand[0][1]
+      }`;
+    return `${this.totalHand[0] !== this.totalHand[1] ? `Showing ${this.totalHand[0]} or ${this.totalHand[1]}` : `Showing ${this.totalHand[0]}`}`
   }
 }
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { BlackjackHand } from "./blackjack";
+import { BlackjackHand, DealerHand } from "./blackjack";
 import { Deck } from "./Deck";
 const prompt = require("prompt-sync")({ sigint: true });
 const win_count = [0, 0]; //[Dealer,Player]
@@ -8,5 +8,28 @@ prompt("Press any key to begin...");
 playing = true;
 
 const deck = new Deck();
-const dealer = new BlackjackHand();
+const dealer = new DealerHand();
 const player = new BlackjackHand();
+
+while (playing) {
+  //Deal Initial Cards
+  dealer.hit(deck);
+  player.hit(deck);
+  dealer.hit(deck);
+  player.hit(deck);
+
+  //Display Cards
+  console.log(dealer.getHandToString());
+  console.log(player.getHandToString());
+
+  //Player Continue
+  var continueHit = prompt("Hit or Stand? (y/n)");
+  while (continueHit === "y") {
+    player.hit(deck);
+    console.log(player.getHandToString());
+    continueHit = prompt("Hit or Stand? (y/n)");
+  }
+
+  dealer.showCard();
+  dealer.getHandToString();
+}
