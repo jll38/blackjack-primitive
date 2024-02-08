16e2d4085b4c97489c6831adde940d4cb0b5b8cc
Julian Lechner <jll38@njit.edu>
2024-02-07 20:40:41 -0500

    Feat: Starting menu

--- index.ts
+++ index.ts
@@ -1,8 +1,35 @@
 import { BlackjackHand, DealerHand } from "./blackjack";
 import { Deck } from "./Deck";
-
+import chalk from "./node_modules/chalk/source/index";
 import { Blackjack } from "./Games";
 
-const BlackjackGame = new Blackjack();
+const prompt = require('prompt-sync')();
 
-BlackjackGame.start();
+const BlackjackGame = new Blackjack();
+class initScreen {
+
+  
+  start(){
+    console.clear();
+    console.log(chalk.yellow("Select the game you want to play:"));
+    console.log("Blackjack");
+    console.log(chalk.gray("Ride the Bus"));
+    console.log(chalk.gray("Poker"))
+    console.log(chalk.gray("Texas Hold Em'"))
+    const selection = prompt(``)
+    switch (selection) {
+      case "Blackjack":
+        BlackjackGame.start();
+        break;
+      default:
+        console.log(chalk.red("Invalid selection. Closing program."))
+        break;
+    }
+  }
+}
+
+const screen = new initScreen();
+
+screen.start();
+
+
